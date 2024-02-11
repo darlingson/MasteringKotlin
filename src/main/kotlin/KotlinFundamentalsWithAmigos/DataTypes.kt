e29824cfa81d19aff739657848e9b67092c26cde
darlingson <dmakuwla@gmail.com>
2024-02-11 18:51:41 +0200

    learnt basic data types

---
+++ src/main/kotlin/KotlinFundamentalsWithAmigos/DataTypes.kt
@@ -0,0 +1,11 @@
+package KotlinFundamentalsWithAmigos
+
+fun main() {
+    var number:Int =10
+    var longNumber:Long = 100L
+    var decimals:Double = 3.14
+    var float:Float = 3.14F
+    var booleans:Boolean = false
+    var string:String = "Darlingson"
+    var chars : Char = 'D'
+}
