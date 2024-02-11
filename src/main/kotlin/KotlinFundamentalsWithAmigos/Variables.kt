1e4a43ea956a51b7d99ad1e434ba1b4db2d76e00
darlingson <dmakuwla@gmail.com>
2024-02-11 18:47:12 +0200

    learnt variables

---
+++ src/main/kotlin/KotlinFundamentalsWithAmigos/Variables.kt
@@ -0,0 +1,12 @@
+package KotlinFundamentalsWithAmigos
+
+fun main() {
+    var name:String = "Darlingson"
+    var age:Int = 28
+
+    println(name)
+    println(age)
+
+    println("Name: ${name} Age: ${age}")
+
+}
