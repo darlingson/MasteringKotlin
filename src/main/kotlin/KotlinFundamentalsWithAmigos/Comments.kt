ebb960f9aca80954ede3c18148bf4f625aeb7762
darlingson <dmakuwla@gmail.com>
2024-02-11 18:40:58 +0200

    learnt comments

---
+++ src/main/kotlin/KotlinFundamentalsWithAmigos/Comments.kt
@@ -0,0 +1,10 @@
+package KotlinFundamentalsWithAmigos
+
+fun main(){
+//    println("Hello Please Comment me")
+    /*
+    this is a
+    multiline comment
+     */
+    print("Non of the above code will run")
+}
