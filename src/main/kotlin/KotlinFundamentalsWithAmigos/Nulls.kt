da1ac7011a7f448b4eaf7e5a310a827a8b4ced6d
darlingson <dmakuwila@gmail.com>
2024-02-21 19:51:57 +0200

    learnt about null safety

---
+++ src/main/kotlin/KotlinFundamentalsWithAmigos/Nulls.kt
@@ -0,0 +1,11 @@
+fun main()
+    var name = "Darlingson"
+    println(name.uppercase())
+
+    var brand:String? = "Darlingson"
+    brand=null
+    println(brand?.uppercase())
+
+    //pointing a string variable is not allowed
+    //to assign null to a variable, use the question
+}
