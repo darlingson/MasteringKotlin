23f36ad19dfed1b64d2987846962c89dffc535e0
darlingson <dmakuwila@gmail.com>
2024-02-21 19:58:04 +0200

    learnt about strings

--- src/main/kotlin/KotlinFundamentalsWithAmigos/Nulls.kt
+++ src/main/kotlin/KotlinFundamentalsWithAmigos/Nulls.kt
@@ -1,10 +1,9 @@
-fun main()
+fun main() {
     var name = "Darlingson"
     println(name.uppercase())
 
     var brand:String? = "Darlingson"
     brand=null
-    println(brand?.uppercase())
 
     //pointing a string variable is not allowed
     //to assign null to a variable, use the question
---
+++ src/main/kotlin/KotlinFundamentalsWithAmigos/Strings.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val name = "Darlingson"
+    println(name)
+    println(name.uppercase())
+    println(name.lowercase())
+    println(name.length)
+    println(name[0])
+    println(name[4])
+
+    println(name.isEmpty())
+    println("".isEmpty())
+
+}
