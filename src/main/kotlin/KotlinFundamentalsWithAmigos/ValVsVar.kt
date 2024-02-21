3d63d353952ee13bc054c5ba4157fba4d07cc3c9
darlingson <dmakuwila@gmail.com>
2024-02-21 19:34:40 +0200

    learnt the difference between var and val

---
+++ src/main/kotlin/KotlinFundamentalsWithAmigos/ValVsVar.kt
@@ -0,0 +1,9 @@
+fun main(){
+    var brandVar = "Darlingson"
+    brandVar = "Makuwila"
+
+    val brandVal = "Darlingson"
+
+    //val is immutable, while var is mutable
+    //immutability is prefered
+}
